2ed14430c71b2ee1260d91e6d57062519bae318a
LeandroRico <lrico@kindalab.co>
2022-11-04 07:24:14 -0300

    customer endpoints created

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const usersRouter = require('./usersRouter');
+const customersRouter = require('./customersRouter');
 
 const routerApi = (app) => {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/users', usersRouter);
+  router.use('/customers', customersRouter);
 };
 
 module.exports = routerApi;
