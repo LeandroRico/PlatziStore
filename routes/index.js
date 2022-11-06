0bc3d24d30ecc64b8f1aa4607cc3644c8f5eebdc
LeandroRico <lrico@kindalab.co>
2022-11-06 11:31:48 -0300

    categories endpoints created

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const usersRouter = require('./usersRouter');
 const customersRouter = require('./customersRouter');
+const categoriesRouter = require('./categoriesRouter');
 
 const routerApi = (app) => {
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/users', usersRouter);
   router.use('/customers', customersRouter);
+  router.use('/categories', categoriesRouter);
 };
 
 module.exports = routerApi;
